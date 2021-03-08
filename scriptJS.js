89339b7b761a1693f1a615b9f54b7e248492ff48
renaudesign <renaud.sourmail@gmail.com>
2021-03-08 11:57:36 +0100

    Javascript exo 4 terminé

---
+++ scriptJS.js
@@ -0,0 +1,4 @@
+// @ts-check
+let number1 = parseInt(prompt("Veuillez indiquer un premier nombre à virgule.", ""))
+let number2 = parseInt(prompt("Veuillez indiquer un second nombre à virgule.", ""))
+alert("le résultat est : " + number1 * number2)
